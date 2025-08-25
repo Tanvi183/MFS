fda707f47c6e7281cef388b63ac0c1f9d7f6bc73
tanvi183 <mneshat7@gmail.com>
2025-08-25 02:27:43 -0700

    create Add Money Feature with DOM

--- js/home.js
+++ js/home.js
@@ -1,1 +1,24 @@
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
 
+    const bank = document.getElementById("bank").value;
+    const accountNumber = document.getElementById("account-number").value;
+    const amountAdd = document.getElementById("amount-add");
+    const amountAddNew = parseInt(amountAdd.value);
+    const bankPinNumber = document.getElementById("bank-pin-number").value;
+
+    // console.log(bank, accountNumber, amountAdd, bankPinNumber);
+
+    //Total Available Balance
+    const availableBalance = parseInt(
+      document.getElementById("available-balance").innerText
+    );
+    const totalavailableBalance = amountAddNew + availableBalance;
+    document.getElementById("available-balance").innerText = totalavailableBalance;
+    amountAdd.value = '';
+
+
+    
+  });
