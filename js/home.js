78bee2b8f678083d0491afd8e98d3402f9fe00bf
tanvi183 <mneshat7@gmail.com>
2025-08-25 22:24:59 -0700

    use funciton to reuse the code

--- js/home.js
+++ js/home.js
@@ -1,15 +1,47 @@
 const validPin = 1234;
 
+// function to get input value with converted
+function getInputValueNumber(id) {
+  const inputField = parseInt(document.getElementById(id).value);
+  return inputField;
+}
+
+// function to get input value
+function getInputValue(id) {
+  const inputField = document.getElementById(id).value;
+  return inputField;
+}
+
+// function to get innertext
+function getInnerText(id) {
+  const element = parseInt(document.getElementById(id).innerText);
+  return element;
+}
+
+// function to set innertext
+function setInnerText(value) {
+  const availableBalance = document.getElementById("available-balance");
+  availableBalance.innerText = value;
+}
+
+// function to clean the input field
+function cleanInputField(id) {
+  const inputField = document.getElementById(id);
+  inputField.value = "";
+}
+
+
+
+// Add money feature
 document
   .getElementById("btn-add-money")
   .addEventListener("click", function (event) {
     event.preventDefault();
 
-    const bank = document.getElementById("bank").value;
-    const accountNumber = document.getElementById("account-number").value;
-    const amountAdd = document.getElementById("amount-add");
-    const amountAddNew = parseInt(amountAdd.value);
-    const bankPinNumber = document.getElementById("bank-pin-number").value;
+    const bank = getInputValue("bank");
+    const accountNumber = getInputValue("account-number");
+    const amountAdd = getInputValueNumber("amount-add");
+    const bankPinNumber = getInputValueNumber("bank-pin-number");
 
     // Is bank account numbers are 11 digit or not
     if (accountNumber.length < 11) {
@@ -17,54 +49,63 @@ document
       return;
     }
 
-    // Pin number is valid or not
+    // // Pin number is valid or not
     if (bankPinNumber != validPin) {
       alert("Please Provide Valid Pin Number");
       return;
     }
 
     // Total Available Balance
-    const availableBalance = parseInt(
-      document.getElementById("available-balance").innerText
-    );
-    const totalavailableBalance = amountAddNew + availableBalance;
-    document.getElementById("available-balance").innerText =
-      totalavailableBalance;
-    amountAdd.value = "";
+    const availableBalance = getInnerText("available-balance");
+
+    const totalavailableBalance = amountAdd + availableBalance;
+
+    setInnerText(totalavailableBalance);
+
+    // Clear the input field
+    cleanInputField("amount-add");
   });
 
 
-// Toggling Features
-document
-  .getElementById("add-money-section")
-  .addEventListener("click", function () {
-    document.getElementById("cash-out-parent").style.display = "none";
-    document.getElementById("add-money-parent").style.display = "block";
-  });
-
-document
-  .getElementById("cash-out-section")
-  .addEventListener("click", function () {
-    document.getElementById("cash-out-parent").style.display = "block";
-    document.getElementById("add-money-parent").style.display = "none";
-  });
-
-
-// Money withdraw
+// Money withdraw Feature
 document
   .getElementById("btn-withdraw-money")
   .addEventListener("click", function (event) {
     event.preventDefault();
 
-    const amountWithdraw = document.getElementById('amount-withdraw');
-    const amountWithdrawNew = parseInt(amountWithdraw.value);
+    const amountWithdraw = getInputValueNumber("amount-withdraw");
 
-    const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+    const availableBalance = getInnerText("available-balance");
 
-    const totalNewAvailableBalance = (availableBalance - amountWithdrawNew);
+    const totalNewAvailableBalance = availableBalance - amountWithdraw;
 
-    document.getElementById('available-balance').innerText = totalNewAvailableBalance;
+    setInnerText(totalNewAvailableBalance);
 
-    amountWithdraw.value = '';
-    
+    cleanInputField("amount-withdraw");
   });
+
+
+
+  
+// Toggling Feature
+function handleToggle(id1, id2) {
+  document.getElementById(id1).addEventListener("click", function () {
+    const forms = document.getElementsByClassName("transaction-form");
+    for (const form of forms) {
+      form.style.display = "none";
+    }
+    document.getElementById(id2).style.display = "block";
+  });
+}
+
+handleToggle("add-money-section", "add-money-parent");
+handleToggle("cash-out-section", "cash-out-parent");
+handleToggle("transfer-money-section", "transfer-money-parent");
+handleToggle("get-bonus-section", "get-bonus-parent");
+handleToggle("pay-bill-section", "pay-bill-parent");
+handleToggle("transactions-section", "transaction-history-parent");
+
+
+
+
+
